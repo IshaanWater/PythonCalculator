4fa940732e951ee3736fb96b3ba3d49f330c1bf5
isi7ha <ishaansinha05@gmail.com>
2026-06-19 02:55:40 -0400

    first commit

---
+++ test.py
@@ -0,0 +1,6 @@
+print("Please tell me two numbers: ") #comment 
+
+num1 = float(input("First number: "))
+num2 = float(input("Second number: "))
+
+print("The sum of the two numbers is: ", num1 + num2)
