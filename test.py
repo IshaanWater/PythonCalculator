6839086bc61dabba2c8de70192f86eb2b03755b2
isi7ha <ishaansinha05@gmail.com>
2026-06-19 03:07:26 -0400

    Updated test.py

--- test.py
+++ test.py
@@ -4,3 +4,4 @@ num1 = float(input("First number: "))
 num2 = float(input("Second number: "))
 
 print("The sum of the two numbers is: ", num1 + num2)
+print("The difference of the two numbers is: ", num1 - num2)
