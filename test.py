3e04e8a28dcc09e49303ccb6110634671ad4f560
isi7ha <ishaansinha05@gmail.com>
2026-06-21 00:10:56 -0400

    More complex

--- test.py
+++ test.py
@@ -3,5 +3,12 @@ print("Please tell me two numbers: ") #comment
 num1 = float(input("First number: "))
 num2 = float(input("Second number: "))
 
-print("The sum of the two numbers is: ", num1 + num2)
-print("The difference of the two numbers is: ", num1 - num2)
+print("You entered: ", num1, "and", num2)
+print("would you like to add or subtract these numbers? (Type 'add' or 'subtract')")
+operation = input("Operation: ")    
+if operation == "add": 
+    final = num1 + num2
+    print("The sum is: ", final)
+elif operation == "subtract" :
+    final = num1 - num2 
+    print("The difference is: ", final)
